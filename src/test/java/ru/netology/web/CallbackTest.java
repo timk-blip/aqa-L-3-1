a207e7264b52ceb79abdae5e8eec73d1ff537127
Timur Kiselev <snygens@mail.ru>
2020-08-27 14:56:15 +0300

    add private static ChromeOptions options v2

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -1,46 +1,33 @@
 package ru.netology.web;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.openqa.selenium.By.cssSelector;
-
 class CallbackTest {
-    private static ChromeOptions options;
     private WebDriver driver;
-
-
+    private static ChromeOptions options;
     @BeforeAll
     static void setUpAll() {
         System.setProperty("webdriver.chrome.driver", "driver/linux/chromedriver");
         options = new ChromeOptions();
         options.addArguments("--headless");
     }
-
-
     @BeforeEach
     void setUp() {
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
     }
-
     @AfterEach
     void tearDown() {
         driver.quit();
         driver = null;
     }
-
     @Test
-    void shouldTestV1 (){
+    void shouldTestV1() {
         driver.get("http://localhost:9999");
         driver.findElement(cssSelector("[type='text']")).sendKeys("ацпуц");
         driver.findElement(cssSelector("[class='checkbox__box']")).click();
